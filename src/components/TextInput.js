30fdc7ba5e431b5341b3e730edd436247502957b
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-02-03 00:02:01 -0700

    eliminated h2 element

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -3,7 +3,6 @@ import React from "react";
 const TextInput = props => {
     return (
         <div id="TextInput">
-            <h2>Translated text appears in p below:</h2>
             <p id="NewText" {...props}></p>
         </div>
     );
