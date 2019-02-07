3f97bd5fb4e5a31ad2ab3b449a6df42909152432
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-02-06 20:52:18 -0700

    updated github link

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,10 +6,10 @@ function About() {
         <>
             <h3>Hello</h3>
            
-            <p>This app is powered by the <a target="blank" href={"https://w3c.github.io/speech-api/"}>Web Speech API</a> and a number of <a target="blank" href={"https://github.com/llcook/web-speech/blob/master/client/package.json"}>front-end</a> and <a target="blank" href={"https://github.com/llcook/web-speech/blob/master/package.json"}>back-end</a> technologies.
+            <p>This app is powered by the <a target="blank" href={"https://w3c.github.io/speech-api/"}>Web Speech API</a> and a number of <a target="blank" href={"https://github.com/llcook/phrase-grabber/blob/master/client/package.json"}>front-end</a> and <a target="blank" href={"https://github.com/llcook/phrase-grabber/blob/master/package.json"}>back-end</a> technologies.
             </p>
 
-            <p>Each <Link to={"/api/notes"}>object</Link> represents one stored instance of speech recognition. Waves of text represent transcriptions stored while in progress.</p>
+            <p>Each stored <Link to={"/api/notes"}>object</Link> represents one instance of speech recognition. Waves of text represent transcriptions stored while in progress.</p>
             
             <p>Future updates will demonstrate live transcription.</p>
 
